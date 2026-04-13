8f346b1118c33c975e734799c93fb51eee5712e2
Ather Hussen Murad <muradnowshad20@gmail.com>
2026-04-13 11:49:44 +0600

    responsive footer added right now

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h2>Footer</h2>
+    <div className="mt-16 flex flex-col items-center md:flex-row gap-8 md:gap-0 md:justify-between md:items-start bg-gray-800 p-8 rounded-lg">
+      <div className="flex flex-col gap-4 items-center md:items-start">
+        <Link href={"/"} className="flex items-center">
+          <Image
+            src={"/logo.png"}
+            alt="TrendLama"
+            width={36}
+            height={36}
+            //   className="w-6 h-6 md:w-9 md:h-9"
+          ></Image>
+        </Link>
+        <p className="hidden md:block font-medium text-shadow-md tracking-wider">
+          TREND-LAMA
+        </p>
+        <p className="text-sm text-gray-400">2026 Trend-Lama</p>
+        <p className="text-sm text-gray-400">All right reserved</p>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Link</p>
+        <Link href={"/"}>Home</Link>
+        <Link href={"/contact"}>Contact</Link>
+        <Link href={"/"}>Terms of Service</Link>
+        <Link href={"/"}>Privacy Policy</Link>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Link</p>
+        <Link href={"/"}>Home</Link>
+        <Link href={"/contact"}>Contact</Link>
+        <Link href={"/"}>Terms of Service</Link>
+        <Link href={"/"}>Privacy Policy</Link>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Link</p>
+        <Link href={"/"}>Home</Link>
+        <Link href={"/contact"}>Contact</Link>
+        <Link href={"/"}>Terms of Service</Link>
+        <Link href={"/"}>Privacy Policy</Link>
+      </div>
     </div>
   );
 };
